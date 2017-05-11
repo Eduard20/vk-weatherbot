b2a906f9a2b802e4984e83486594558377ed4c78
Eduard Simonyan <esimonyan2014@gmail.com>
2017-05-11 17:22:58 +0400

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+
+const config = {
+    message : "some message",
+    vkConf : {
+        token: "0fcff0bea59b3effbfb8c0f6efebd331f8637b5f13e543cd4e30145db6c5f2f55a1eb01df80b2b0146576",
+        call: 'execute',
+        restartCount: 2,
+        restartWait: 1500,
+    }
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,35 @@
+
+/**
+ * Module dependencies
+ */
+
+const VK = require('vk-io');
+const config = require("./config");
+const request = require("request");
+
+const vk = new VK(config.vkConf);
+
+vk.longpoll.start()
+    .then(() => {
+        console.log('Long Poll запущен');
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+vk.longpoll.on("message", (msg) => {
+    if (msg.flags.indexOf('outbox') !== -1) {
+        return;
+    }
+    request(`http://api.openweathermap.org/data/2.5/weather?q=${msg.text}&APPID=${config.key}&units=metric`, (err, res, body) => {
+        if (err) return console.error(err);
+        let json = JSON.parse(body);
+        let result;
+        if (json.cod != 200) {
+            return msg.send("Please provide existing city");
+        }
+        if (json.main.temp > 0) {
+            result = `+${json.main.temp}, ${json.weather[0].main}, ${json.weather[0].description}, ${json.name}, ${json.sys.country}`;
+        }
+        msg.send(result);
+    });
+});
